860d180ec5b67d836b6e79008de762a707b2047d
takaosecond <takaosecond@icloud.com>
2022-10-10 14:46:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ marginBottom: "20px" }}>Trello風アプリ</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
